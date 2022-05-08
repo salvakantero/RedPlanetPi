fc3b26c8f2abcafbad2324ca3e8bb41964674fb0
salvakantero <salvacantero@outlook.com>
2022-05-08 22:33:04 +0200

    -

---
+++ src/rrplanet.py
@@ -0,0 +1,13 @@
+# ======================================================================
+# Raspberry-Red Planet v1.0
+# salvaKantero 2022
+# ======================================================================
+
+import pygame # pygame library functions
+#import sys 
+#import os
+#import json
+from pygame.locals import *
+from pygame.constants import (QUIT, K_UP, K_DOWN, K_LEFT, K_RIGHT) 
+
+from map import map # The 30 screens of the map.
