c4277a4b17fed0c7df5af420446d15b2f5e494a2
salvakantero <salvacantero@outlook.com>
2022-05-16 11:57:22 +0200

    -

--- src/rrplanet.py
+++ src/rrplanet.py
@@ -9,7 +9,7 @@ import os # file operations
 import json # json file management
 import sys # exit()
 from pygame.locals import * # allows constants without typing "pygame."
-from pygame.constants import (QUIT, KEYDOWN, K_ESCAPE, K_LEFT, K_RIGHT) 
+from pygame.constants import (QUIT, KEYDOWN, K_ESCAPE) 
 
 
 
@@ -19,8 +19,25 @@ from pygame.constants import (QUIT, KEYDOWN, K_ESCAPE, K_LEFT, K_RIGHT)
 
 bp = os.path.dirname(__file__) + "/" # exec path (+ "/" when using VS Code)
 jp = os.path.join # forms the folder/file path
-base_screen_size = [720, 480] # size of the main window
-map_number = last_map = 0 # current map number
+
+base_screen_size = 720, 480 # main window size
+map_size = 240, 160 # map size (unscaled)
+
+map_number = 0 # required map number
+last_map = -1 # last map loaded
+
+
+
+#===============================================================================
+# Generic functions
+#===============================================================================
+
+# get a value from a dictionary
+def FindData(lst, key, value):
+    for i, dic in enumerate(lst):
+        if dic[key] == value:
+            return dic
+    return -1
 
 
 
@@ -36,20 +53,12 @@ def LoadMap(map_number):
 
 
 
-# get a value from a dictionary
-def FindData(lst, key, value):
-    for i, dic in enumerate(lst):
-        if dic[key] == value:
-            return dic
-    return -1
-
-
-
 # dump tiled map into 'mapdata'
 def ProcessMap(map_file):
     # reads the entire contents of the json
     with open(map_file) as json_data:
         data_readed = json.load(json_data)
+
     # gets the map dimensions
     data = {"width": data_readed["width"], "height": data_readed["height"]}
     # gets a list of all tiles
@@ -59,6 +68,7 @@ def ProcessMap(map_file):
     for x in range(0, data["height"]):
         st = x * data["width"]
         data["data"].append(raw_data[st: st + data["width"]])
+
     # gets the name of the tile file
     tileset = data_readed["tilesets"][0]["source"].replace(".tsx",".json")
     # gets the data from the tile file
@@ -90,24 +100,25 @@ def DrawMap():
 
 
 #===============================================================================
-# Initialisation
+# Main
 #===============================================================================
 
+# Initialisation
 pygame.init()
-# generates a 720x480 window with title and 32-bit colour.
-screen = pygame.display.set_mode(base_screen_size, 0, 32)
+# generates a main window with title, icon, and 32-bit colour.
+screen = pygame.display.set_mode(base_screen_size, DOUBLEBUF, 32)
 pygame.display.set_caption(".:: Raspberry-Red Planet ::.")
+icon = pygame.image.load(jp(bp, "images/icon.png")).convert_alpha()
+pygame.display.set_icon(icon)
 # area covered by the map
-display = pygame.Surface((240, 160))
+display = pygame.Surface(map_size)
 # clock to control the FPS
 clock = pygame.time.Clock()
+# menu music
+pygame.mixer.music.load(jp(bp, "sounds/ingame.ogg"))
+pygame.mixer.music.play()
 
-
-
-#===============================================================================
 # Main loop
-#===============================================================================
-
 while True:
     # event management
     for event in pygame.event.get():
@@ -118,21 +129,14 @@ while True:
                 pygame.quit()
                 sys.exit()
 
-    # ================================== TEMP
-            if event.key == K_RIGHT:
-                map_number = 1
-            elif event.key == K_LEFT:
-                map_number = 0
-
+    # change map if neccessary
     if (map_number != last_map):
         LoadMap(map_number)
         last_map = map_number
-    # ================================== TEMP
 
     # scale x 3 the map and transfer to screen
     screen.blit(pygame.transform.scale(display, base_screen_size), 
     ((screen.get_width() - base_screen_size[0]) // 2, 
     (screen.get_height() - base_screen_size[1]) // 2))
-
     pygame.display.update() # refreshes the screen
     clock.tick(60) # 60 FPS
