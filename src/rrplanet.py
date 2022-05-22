680ed7b4a85bf47f85c6f9eec6e34567e4c2e5b4
salvakantero <salvacantero@outlook.com>
2022-05-22 14:19:06 +0200

    -

--- src/rrplanet.py
+++ src/rrplanet.py
@@ -9,7 +9,7 @@ import os # file operations
 import json # json file management
 import sys # exit()
 from pygame.locals import * # allows constants without typing "pygame."
-from pygame.constants import (QUIT, KEYDOWN, K_ESCAPE) 
+from pygame.constants import (QUIT, KEYDOWN, K_ESCAPE, K_LEFT, K_RIGHT) 
 
 
 
@@ -20,8 +20,9 @@ from pygame.constants import (QUIT, KEYDOWN, K_ESCAPE)
 bp = os.path.dirname(__file__) + "/" # exec path (+ "/" when using VS Code)
 jp = os.path.join # forms the folder/file path
 
-base_screen_size = 720, 480 # main window size
-map_size = 240, 160 # map size (unscaled)
+win_size = 800, 600 # main window size
+map_scaled_size = 720, 480 # map size (scaled)
+map_unscaled_size = 240, 160 # map size (unscaled)
 
 map_number = 0 # required map number
 last_map = -1 # last map loaded
@@ -84,7 +85,6 @@ def LoadMap(map_number):
     global map_data
     map_data = ProcessMap(jp(bp, "maps/map" + str(map_number) + ".json"))
     DrawMap()
-    DrawMapTitle(map_number)
 
 
 
@@ -130,13 +130,7 @@ def DrawMap():
             tile = pygame.image.load(jp(bp, "images/" + t["image"])).convert()
             tileRect = tile.get_rect()
             tileRect.topleft = (x * t["imagewidth"], y * t["imageheight"])   
-            display.blit(tile, tileRect)
-
-
-
-# print the screen title at the top of the screen
-def DrawMapTitle(map_number):
-    pass
+            map_display.blit(tile, tileRect)
 
 
 
@@ -148,12 +142,12 @@ def DrawMapTitle(map_number):
 pygame.init()
 pygame.mixer.init()
 # generates a main window with title, icon, and 32-bit colour.
-screen = pygame.display.set_mode((800, 600), 0, 32)
+screen = pygame.display.set_mode(win_size, 0, 32)
 pygame.display.set_caption(".:: Raspberry-Red Planet ::.")
 icon = pygame.image.load(jp(bp, "images/icon.png")).convert_alpha()
 pygame.display.set_icon(icon)
 # area covered by the map
-display = pygame.Surface(map_size)
+map_display = pygame.Surface(map_unscaled_size)
 # clock to control the FPS
 clock = pygame.time.Clock()
 
@@ -187,8 +181,8 @@ while True:
         last_map = map_number
 
     # scale x 3 the map and transfer to screen
-    screen.blit(pygame.transform.scale(display, base_screen_size), 
-    ((screen.get_width() - base_screen_size[0]) // 2, # horizontally centred
-    screen.get_height() - base_screen_size[1] - 20)) # room for the scoreboard
+    screen.blit(pygame.transform.scale(map_display, map_scaled_size), 
+    ((screen.get_width() - map_scaled_size[0]) // 2, # horizontally centred
+    screen.get_height() - map_scaled_size[1] - 20)) # room for the scoreboard
     pygame.display.update() # refreshes the screen
     clock.tick(60) # 60 FPS
