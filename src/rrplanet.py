9491ebf46aed12ed2674e7f912fc9bc3dcb33ae0
salvakantero <salvacantero@outlook.com>
2022-06-01 18:55:04 +0200

    -

--- src/rrplanet.py
+++ src/rrplanet.py
@@ -4,6 +4,7 @@
 # salvaKantero 2022
 # ==============================================================================
 
+from turtle import window_height
 import pygame # pygame library functions
 import os # file operations
 import json # json file management
@@ -63,6 +64,7 @@ map_names = {
 }
 
 
+
 #===============================================================================
 # Generic functions
 #===============================================================================
@@ -73,6 +75,26 @@ def FindData(lst, key, value):
         if dic[key] == value:
             return dic
     return -1
+
+
+
+def clip(surf,x,y,x_size,y_size):
+    handle_surf = surf.copy()
+    clipR = pygame.Rect(x,y,x_size,y_size)
+    handle_surf.set_clip(clipR)
+    image = surf.subsurface(handle_surf.get_clip())
+    return image.copy()
+
+
+
+# cambia el color transparente de una imagen
+def swap_color(img,old_c,new_c):
+    global e_colorkey
+    img.set_colorkey(old_c)
+    surf = img.copy()
+    surf.fill(new_c)
+    surf.blit(img,(0,0))
+    return surf
 
 
 
@@ -135,6 +157,109 @@ def DrawMap():
 
 
 #===============================================================================
+# Font functions
+#===============================================================================
+
+# carga las letras de la imagen de fuente
+def load_font_img(path, font_color):
+    fg_color = (255, 0, 0) # rojo
+    bg_color = (0, 0, 0) # negro
+    font_img = pygame.image.load(jp(bp,path)).convert() # carga la imagen de fuente
+    font_img = swap_color(font_img, fg_color, font_color) # aplica el color de fuente solicitado
+    last_x = 0
+    letters = []
+    letter_spacing = []
+    for x in range(font_img.get_width()): # para todo el ancho de la imagen
+        if font_img.get_at((x, 0))[0] == 127: # separador rojo
+            # guarda en el array la porción de imagen con la letra que nos interesa
+            letters.append(clip(font_img, last_x, 0, x - last_x, font_img.get_height()))
+            # guarda el ancho de la letra
+            letter_spacing.append(x - last_x)
+            last_x = x + 1
+        x += 1
+    for letter in letters:
+        letter.set_colorkey(bg_color)  # aplica color de fondo a cada letra del array
+    return letters, letter_spacing, font_img.get_height()
+
+
+
+# crea una fuente nueva a partir de una ruta de imagen y un color
+class Font():
+    def __init__(self, path, color):
+        self.letters, self.letter_spacing, self.line_height = load_font_img(path, color)
+        self.font_order = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','.','-',',',':','+','\'','!','?','0','1','2','3','4','5','6','7','8','9','(',')','/','_','=','\\','[',']','*','"','<','>',';']
+        self.space_width = self.letter_spacing[0]
+        self.base_spacing = 1
+        self.line_spacing = 2
+
+    # retorna ancho de un texto
+    def width(self, text):
+        text_width = 0
+        for char in text:
+            if char == ' ':
+                text_width += self.space_width + self.base_spacing
+            else:
+                text_width += self.letter_spacing[self.font_order.index(char)] + self.base_spacing
+        return text_width
+
+    # pinta texto
+    def render(self, text, surf, loc, line_width=0):
+        x_offset = 0
+        y_offset = 0
+        if line_width != 0:
+            spaces = []
+            x = 0
+            # ?
+            for i, char in enumerate(text):
+                if char == ' ':
+                    spaces.append((x, i))
+                    x += self.space_width + self.base_spacing
+                else:
+                    x += self.letter_spacing[self.font_order.index(char)] + self.base_spacing
+            line_offset = 0
+            # ?
+            for i, space in enumerate(spaces):
+                if (space[0] - line_offset) > line_width:
+                    line_offset += spaces[i - 1][0] - line_offset
+                    if i != 0:
+                        text = text[:spaces[i - 1][1]] + '\n' + text[spaces[i - 1][1] + 1:]
+        for char in text:
+            if char not in ['\n', ' ']:
+                # pinta la letra y suma el ancho
+                surf.blit(self.letters[self.font_order.index(char)], (loc[0] + x_offset, loc[1] + y_offset))
+                x_offset += self.letter_spacing[self.font_order.index(char)] + self.base_spacing
+            elif char == ' ':
+                x_offset += self.space_width + self.base_spacing
+            else: # salto de línea
+                y_offset += self.line_spacing + self.line_height
+                x_offset = 0
+    
+
+
+# pinta texto con borde
+def outlined_text(bg_font, fg_font, t, surf, pos):
+    bg_font.render(t, surf, [pos[0] - 1, pos[1]])
+    bg_font.render(t, surf, [pos[0] + 1, pos[1]])
+    bg_font.render(t, surf, [pos[0], pos[1] - 1])
+    bg_font.render(t, surf, [pos[0], pos[1] + 1])
+    fg_font.render(t, surf, [pos[0], pos[1]])
+
+
+
+#===============================================================================
+# Auxiliar functions
+#===============================================================================
+
+# scanlines
+def ApplyFilter():
+    j = 0
+    while j < win_size[0]:
+        j+=3
+        pygame.draw.line(screen, (15,15,15), (40,j), (760, j))
+
+
+
+#===============================================================================
 # Main
 #===============================================================================
 
@@ -146,6 +271,9 @@ screen = pygame.display.set_mode(win_size, 0, 32)
 pygame.display.set_caption(".:: Raspi-Red Planet ::.")
 icon = pygame.image.load(jp(bp, "images/icon.png")).convert_alpha()
 pygame.display.set_icon(icon)
+# fonts
+main_font = Font('images/small_font.png', (168, 217, 227)) # texto celeste
+bg_font = Font('images/small_font.png', (28, 17, 24)) # texto casi negro
 # area covered by the map
 map_display = pygame.Surface(map_unscaled_size)
 # clock to control the FPS
@@ -180,16 +308,20 @@ while True:
         LoadMap(map_number)
         last_map = map_number
 
+    # test 1
+    outlined_text(bg_font, main_font, 'Level - ' + str(map_number), map_display, (8, 5))
+    bg_font.render('Thanks for playing!', map_display, (200 - 1, map_display.get_height() // 3))
+    bg_font.render('Thanks for playing!', map_display, (200 + 1, map_display.get_height() // 3))
+    bg_font.render('Thanks for playing!', map_display, (200, map_display.get_height() // 3 - 1))
+    bg_font.render('Thanks for playing!', map_display, (200, map_display.get_height() // 3 + 1))
+    main_font.render('Thanks for playing!', map_display, (200, map_display.get_height() // 3))
+
     # scale x 3 the map and transfer to screen
     screen.blit(pygame.transform.scale(map_display, map_scaled_size), 
     ((screen.get_width() - map_scaled_size[0]) // 2, # horizontally centred
     screen.get_height() - map_scaled_size[1] - 20)) # room for the scoreboard
 
-    # scanlines
-    j = 0
-    for i in range(192):
-        j+=3
-        pygame.draw.line(screen, (15,15,15), (40,j), (760, j))
+    ApplyFilter() # scanlines
 
     pygame.display.update() # refreshes the screen
     clock.tick(60) # 60 FPS
