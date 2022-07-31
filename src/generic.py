081a39a3cd2a6ec53dd8836be3ecc79fd1fb7b64
salvakantero <salvacantero@outlook.com>
2022-07-31 17:02:47 +0200

    -

--- src/generic.py
+++ src/generic.py
@@ -35,8 +35,8 @@ def swap_color(img,old_c,new_c):
 
 
 # scanlines
-def apply_scanlines(screen, height, rgb):
+def apply_scanlines(surface, height, rgb):
     j = 0
     while j < height:
         j+=3
-        pygame.draw.line(screen, (rgb, rgb, rgb), (40, j), (760, j))
+        pygame.draw.line(surface, (rgb, rgb, rgb), (40, j), (760, j))
